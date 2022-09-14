4cf62885c203f508f8ba4bce565c263fc0bf446d
ShahaMoh <112881103+ShahaMoh@users.noreply.github.com>
2022-09-14 20:11:58 +0300

    cw5

---
+++ CW5-pa1.js
@@ -0,0 +1,27 @@
+
+
+alert ('مرحبا بك في موقعي انا اسمي  شاههوسوف احسب درجتك ');
+
+let grade = prompt("ادخل درجتك");
+console.log ("grade");
+if ( grade >= 90&& grade <=100)
+{
+    console.log( "  حلصت علي امتياز");
+
+}
+else if( grade >=80 && grade <=89)
+{
+    console.log(" حصلت علي جيد جدا");
+}
+  
+else if( grade >=70&&  grade <= 79)
+{
+    console.log("احلت علي جيد ");}
+
+    else if( grade >=60 && grade <= 69)
+{
+    console.log("حصلت علي مقبول ");}
+
+    else if( grade >=50 )
+{
+    console.log("انا اسف لقد رسبت ");}
